d841635053faae568e4060a7d6d5587352d2bb5b
Maxi <herreramaxi@gmail.com>
2023-02-07 23:40:21 +0000

    API calls using Auth0 token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { MessageService } from './core/services';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AIaaS_UI';
+
+  message = '';
+  isAuthenticated$ = this.auth.isAuthenticated$;
+  constructor(private auth: AuthService, public messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.messageService.getAdminResource().subscribe((response) => {
+      const { data, error } = response;
+
+      if (data) {
+        this.message = JSON.stringify(data, null, 2);
+      }
+
+      if (error) {
+        this.message = JSON.stringify(error, null, 2);
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,33 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { AuthModule } from '@auth0/auth0-angular';
+import { AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular';
 import { environment as env } from '../environments/environment';
+import { AppComponent } from './app.component';
 import { LoginButtonComponent } from './login-button/login-button.component';
 import { LogoutButtonComponent } from './logout-button/logout-button.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginButtonComponent,
-    LogoutButtonComponent
+    LogoutButtonComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
-    // Import the module into the application, with configuration
+    HttpClientModule,
     AuthModule.forRoot({
       ...env.auth0,
       httpInterceptor: {
-        allowedList: [`${env.api.serverUrl}/api/messages/admin`, `${env.api.serverUrl}/api/messages/protected`],
+        allowedList: [`${env.api.serverUrl}/api/messages/admin`, `${env.api.serverUrl}/api/messages/protected`]      
       },
     }),
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
