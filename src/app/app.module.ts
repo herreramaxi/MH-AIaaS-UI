bf460b3082e170aed0c3b7173326da257b2e8c62
Maxi <herreramaxi@gmail.com>
2023-02-10 23:53:53 +0000

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular';
 import { environment as env } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthGuardByRole } from './infrastructure/auth-guard-by-role';
+import { HeaderComponent } from './layout/header/header.component';
 import { LoginButtonComponent } from './login-button/login-button.component';
 import { LogoutButtonComponent } from './logout-button/logout-button.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { AppRoutingModule } from './app-routing.module';
 import { PublicComponent } from './public/public.component';
-import { AuthGuardByRole } from './infrastructure/auth-guard-by-role';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +28,7 @@ import { AuthGuardByRole } from './infrastructure/auth-guard-by-role';
     LogoutButtonComponent,
     UserProfileComponent,
     PublicComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,13 +36,22 @@ import { AuthGuardByRole } from './infrastructure/auth-guard-by-role';
     AuthModule.forRoot({
       ...env.auth0,
       httpInterceptor: {
-        allowedList: [`${env.api.serverUrl}/api/messages/admin`, `${env.api.serverUrl}/api/messages/protected`]      
+        allowedList: [`${env.api.serverUrl}/api/messages/admin`, `${env.api.serverUrl}/api/messages/protected`]
       },
     }),
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatNativeDateModule,
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },    
+    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
     AuthGuardByRole
   ],
   bootstrap: [AppComponent]
