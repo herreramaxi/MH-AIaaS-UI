e028a59910483c4c7ec50ed4d7d95326789042c8
Maxi <herreramaxi@gmail.com>
2023-04-08 19:48:15 +0100

    add .env on container

--- deploy.sh
+++ deploy.sh
@@ -14,4 +14,15 @@ then
 fi
 
 docker create -p $PUBLIC_PORT:$INTERNAL_PORT --name $CONTAINER_NAME $IMAGE_NAME:$IMAGE_TAG
+
+rm .env
+touch .env
+echo "AUTH0_DOMAIN=$AUTH0_DOMAIN" | tee -a .env
+echo "AUTH0_CLIENT_ID=$AUTH0_CLIENT_ID" | tee -a .env
+echo "AUTH0_AUDIENCE=$AUTH0_AUDIENCE" | tee -a .env
+echo "AUTH0_CALLBACK_URL=$AUTH0_CALLBACK_URL" | tee -a .env
+echo "API_SERVER_URL=$API_SERVER_URL" | tee -a .env
+docker cp .env $CONTAINER_NAME:/app/.env
+#TODO: remove .env
+
 docker start $CONTAINER_NAME
