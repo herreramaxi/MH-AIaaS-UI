eb9dbe156df83203994eafc2ba08096d1afa8ed3
Maxi <herreramaxi@gmail.com>
2023-04-09 18:15:58 +0100

    test api

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { map } from 'rxjs';
+import { MessageService } from '../core/services';
 
 @Component({
   selector: 'app-user-profile',
@@ -8,9 +9,26 @@ import { map } from 'rxjs';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
+  constructor(private auth: AuthService, private messageService: MessageService) {
+
+    messageService.getAdminResource().subscribe(data => {
+      console.log("getAdminResource")
+      console.log(data)
+    })
+
+    messageService.getProtectedResource().subscribe(data => {
+      console.log("getProtectedResource")
+      console.log(data)
+    })
+
+    messageService.getPublicResource().subscribe(data => {
+      console.log("getPublicResource")
+      console.log(data)
+    })
+  }
 
   user$ = this.auth.user$;
   code$ = this.user$.pipe(map((user) => JSON.stringify(user, null, 2)));
 
-  constructor(private auth: AuthService) { }
+
 }
