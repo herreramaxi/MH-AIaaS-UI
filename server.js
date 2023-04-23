22da3c16456c0a9c25f0a3df33ff46486b94b338
Maxi <herreramaxi@gmail.com>
2023-04-23 21:04:12 +0100

    add redirect to https

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log("starting development mode....")
+    app.use(cors());
+}
+else {
+    console.log("starting production mode....")
+
+    var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+    const ignoreHosts = [];
+    const ignoreRoutes = [];
+    app.use(redirectToHTTPS(ignoreHosts, ignoreRoutes));
+}
+
 app.use(express.static(__dirname + '/dist/aiaa-s-ui'));
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/aiaa-s-ui/index.html'));
