c68106c237488a37236b7a983af533a03dc3348b
Maxi <herreramaxi@gmail.com>
2023-07-01 12:16:40 +0100

    upgrade to Angular 16

--- src/app/infrastructure/auth-guard-by-role.ts
+++ src/app/infrastructure/auth-guard-by-role.ts
@@ -1,10 +1,10 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { AuthService } from "@auth0/auth0-angular";
 import { Observable, tap } from "rxjs";
 
 @Injectable()
-export class AuthGuardByRole implements CanActivate {
+export class AuthGuardByRole  {
    constructor(public auth: AuthService, public router: Router) {
    }
 
